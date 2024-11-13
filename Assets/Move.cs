461ed7419f22f4dda956ac824f337e43b3006513
xulioxesus <julio.leon@firescalestudios.com>
2024-11-13 15:31:27 +0100

    S0202Solution

---
+++ Assets/Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+
+    void Start() {
+
+        this.transform.Translate(6, 0, 2);
+    }
+
+    private void Update() {
+
+    }
+}
