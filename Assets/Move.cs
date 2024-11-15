c8b6a73ed29cb693716155b4f9c5d378f7124010
xulioxesus <julio.leon@firescalestudios.com>
2024-11-15 12:28:45 +0100

    S0206SolutionFinalizado

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -3,13 +3,16 @@
 public class Move : MonoBehaviour {
 
     public GameObject goal;
+    Vector3 direction;
+    public float speed = 5f;
 
-    void Start() {
+    private void LateUpdate() {
+        direction = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
 
-        this.transform.Translate(6, 0, 2);
-    }
-
-    private void Update() {
-
+        if(direction.magnitude > 2){
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
     }
 }
